52a9c0b86c2188a297bb9013ec8f91d515bcc958
Dave Hein <jenesuispasdave@gmail.com>
2016-05-23 18:54:40 -0500

    Use 'dv_' prefix for data volume tags
    Also demonstrated share port with multi-target jenkins job.
    Worked great.

--- docker-book/ch06-testing-with-docker/ci/jenkins/run.sh
+++ docker-book/ch06-testing-with-docker/ci/jenkins/run.sh
@@ -1,12 +1,6 @@
 #! /bin/bash
 #
-# Create and populate the data container
-#
-# docker run --name jenkins_workspace \
-#   -v /opt/jenkins/data/workspace gliderlabs/alpine:latest \
-#   /bin/true
-
 docker run -d -p 8080:8080 --name jenkins \
-  --volumes-from jenkins_workspace \
+  --volumes-from dv_jenkins_workspace \
   -v /var/run/docker.sock:/var/run/docker.sock \
   jamtur01/jenkins-dockercli
